1e29eed76ac36f0107632140c2e5d6b966e36ba9
Pjotr Savitski <gnum@tlu.ee>
2015-01-19 15:30:53 +0200

    LL-984 #close Entity removed from recommendable types.

--- js/view/toolbar/ActivityStreamToolbarView.js
+++ js/view/toolbar/ActivityStreamToolbarView.js
@@ -175,7 +175,7 @@ define(['logger', 'tracker', 'underscore', 'jquery', 'backbone', 'spin', 'voc', 
             var data = {
                     forUser : userParams.user,
                     maxResources : 20,
-                    typesToRecommOnly : ['entity', 'file', 'evernoteResource', 'evernoteNote', 'evernoteNotebook']
+                    typesToRecommOnly : ['file', 'evernoteResource', 'evernoteNote', 'evernoteNotebook']
                 },
                 promise = EntityData.getRecommResources(data);
 
