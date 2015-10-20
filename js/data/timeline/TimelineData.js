09319f171e6e4b11f1523715a6aaecdbfe50f8dd
Pjotr Savitski <gnum@tlu.ee>
2015-10-20 16:21:44 +0300

    LL-1234 Changed timers for fetchData and stateSet, using lookup structure now.
    The issue is that we are dealing with a singleton that handles all of
    the models for TIMELINEs, this is why those were interfearing with each
    other. New approach does not let that happen. NB! Also made a change to
    the timelineStateSet call, it no longer changes the ID/Subject of the
    TIMELINE to correspond to the new id of the current TimelineState
    entity. These just keep their internal identifiers and those do not
    change. This was causing issues for the lookup.

--- js/data/timeline/TimelineData.js
+++ js/data/timeline/TimelineData.js
@@ -1,19 +1,19 @@
 define(['logger', 'voc', 'underscore', 'data/Data', 'data/episode/UserData' ], function(Logger, Voc, _, Data, UserData){
     var m = Object.create(Data);
-    var fetchDataTimer = null;
-    var setStateTimer = null;
     m.init = function(vie) {
         this.LOG.debug("initialize TimelineData");
         this.vie = vie;
         this.vie.entities.on('add', this.filter, this);
         this.setIntegrityCheck(Voc.belongsToVersion, Voc.VERSION, Voc.hasWidget);
         this.setIntegrityCheck(Voc.belongsToUser, Voc.USER);
+        this.timerLookup = {};
     };
     m.LOG = Logger.get('TimelineData');
     /** 
      * Filters entities from added entities to vie.entities
      */
     m.filter= function(model, collection, options) {
+        var that = m;
         if(model.isof(Voc.TIMELINE)){
             this.checkIntegrity(model, options);
             var version = model.get(Voc.belongsToVersion);
@@ -22,7 +22,6 @@ define(['logger', 'voc', 'underscore', 'data/Data', 'data/episode/UserData' ], f
                 var dataSet = {};
                 dataSet[Voc.start] = timelineState.get(Voc.start);
                 dataSet[Voc.end] = timelineState.get(Voc.end);
-                // XXX Timeline data is not preloded, probably some issue with these times
                 model.set(dataSet);
             });
 
@@ -30,34 +29,38 @@ define(['logger', 'voc', 'underscore', 'data/Data', 'data/episode/UserData' ], f
             // TODO resolve this hack: only fire on start change to avoid double execution
             model.on('change:' + this.vie.namespaces.uri(Voc.start),
                 function(model, value, options){
-                    if (fetchDataTimer) {
-                        clearTimeout(fetchDataTimer);
+                    var currentTimer = that.getTimerValue(model.getSubject(), 'fetchData');
+                    if (currentTimer) {
+                        clearTimeout(currentTimer);
                     }
 
-                    fetchDataTimer = setTimeout(function() {
-                        fetchDataTimer = null;
-                        console.log('fetched', value, model.get(Voc.end));
+                    currentTimer = setTimeout(function() {
+                        that.clearTimerValue(model.getSubject(), 'fetchData');
                         UserData.fetchRange(user, value, model.get(Voc.end));
                     }, 250);
+                    that.setTimerValue(model.getSubject(), 'fetchData', currentTimer);
                 }
             );
             model.sync = this.sync;
         }
     };
     m.sync= function(method, model, options) {
+        var that = m;
         m.LOG.debug("sync entity " + model.getSubject() + " by " + method);
         if( !options ) options = {};
         switch(method) {
             case 'create':
             case 'update':
-                if (setStateTimer) {
-                        clearTimeout(setStateTimer);
+                var currentTimer = that.getTimerValue(model.getSubject(), 'setState');
+                if (currentTimer) {
+                        clearTimeout(currentTimer);
                     }
 
-                    setStateTimer = setTimeout(function() {
-                        setStateTimer = null;
+                    currentTimer = setTimeout(function() {
+                        that.clearTimerValue(model.getSubject(), 'setState');
                         m.saveTimelineState(model, options);
                     }, 250);
+                    that.setTimerValue(model.getSubject(), 'setState', currentTimer);
                 break;
             case 'read':
                 m.fetchTimelineState(model, options);
@@ -81,7 +84,9 @@ define(['logger', 'voc', 'underscore', 'data/Data', 'data/episode/UserData' ], f
                         'endTime' : model.get(Voc.end)
                     }
                 }).to('sss').execute().success(function(savedEntityUri) {
-                    model.set(model.idAttribute, savedEntityUri, options);
+                    // XXX This one updates timeline ID/Subject to URI of current state
+                    // Not sure what is the purpose of it
+                    //model.set(model.idAttribute, savedEntityUri, options);
                     if(options.success) {
                         options.success(model);
                     }
@@ -132,6 +137,15 @@ define(['logger', 'voc', 'underscore', 'data/Data', 'data/episode/UserData' ], f
         this.vie.entities.addOrUpdate(newTimeline);
         newTimeline.save();
         return newTimeline;
+    };
+    m.getTimerValue = function(uri, type) {
+        return this.timerLookup[uri + ':' + type];
+    };
+    m.setTimerValue = function(uri, type, value) {
+        this.timerLookup[uri + ':' + type] = value;
+    };
+    m.clearTimerValue = function(uri, type) {
+        delete this.timerLookup[uri + ':' + type];
     };
     return m;
 
