63cc88babf1c980dfda1edc2d884ae357d9dcd28
Pjotr Savitski <gnum@tlu.ee>
2016-04-07 14:30:52 +0300

    LL-1338 Full structure and texts.
    Added texs and full videos structure. Many videos are missing, because
    they do not seem to be on YouTube channel and publicly available.

--- help/js/app.js
+++ help/js/app.js
@@ -1,9 +1,11 @@
-define(['underscore', 'jquery', 'text!../data/help.json', 'colorbox', 'bootstrap'],
-function(_, $, help) {
+define(['underscore', 'jquery', 'text!../data/help.json', 'text!../js/templates/navbar.tpl', 'text!../js/templates/overview_and_structure.tpl', 'colorbox', 'bootstrap'],
+function(_, $, help, NavbarTemplate, OverviewAndStructureTemplate) {
     $(document).ready(function(){
         var helpData = JSON.parse(help),
             appContainerElement = $(document).find('#bnp-help-app'),
-            helpText = '';
+            helpText = '',
+            navbar = _.template(NavbarTemplate, {}),
+            overviewAndStructure = _.template(OverviewAndStructureTemplate, {});
 
         _.each(helpData.apps, function(app) {
             helpText += '<h2>' + app.title + '</h2>';
@@ -17,21 +19,28 @@ function(_, $, help) {
             _.each(app.chapters, function(chapter) {
                 helpText += '<h3>' + chapter.title + '</h3>';
 
-                helpText += '<ul class="list-group">';
-                _.each(chapter.videos, function(video) {
-                    helpText += '<li class="list-group-item">';
-                    helpText += '<a href="' + video.url + '" class="youtube">';
-                    helpText += '<i class="fa fa-youtube"></i>';
-                    helpText += video.title;
-                    helpText += '</a>';
-                    helpText += '<span class="badge">' + video.duration + '</span>';
-                    helpText += '</li>';
-                });
-                helpText += '</ul>';
+                if ( chapter.template && chapter.template === 'overview_and_structure' ) {
+                    helpText += overviewAndStructure;
+                }
+
+                if ( chapter.videos && _.isArray(chapter.videos) && chapter.videos.length > 0 ) {
+                    helpText += '<ul class="list-group">';
+                    _.each(chapter.videos, function(video) {
+                        helpText += '<li class="list-group-item">';
+                        helpText += '<a href="' + video.url + '" class="youtube">';
+                        helpText += '<i class="fa fa-youtube"></i>';
+                        helpText += video.title;
+                        helpText += '</a>';
+                        helpText += '<span class="badge">' + video.duration + '</span>';
+                        helpText += '</li>';
+                    });
+                    helpText += '</ul>';
+                }
             });
         });
 
         appContainerElement.find('.bnp-help-loading').hide();
+        appContainerElement.parent().prepend(navbar);
         appContainerElement.append(helpText);
 
         $('.youtube').colorbox({
---
+++ help/js/templates/navbar.tpl
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <p class="navbar-brand"><img src="../img/logo48.png" width="24" height="24" alt="Learning Layers"/> Bits And Pieces Help</p>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+      </ul>
+      </p>
+
+      <p class="navbar-text navbar-right">
+        <a href="old" class="navbar-link" target="_blank" title="Old version of Help">
+            Prefer Textual Help? Then check out previous version!
+        </a>
+      </p>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
