3fe1330e03a67ce7ce379d31fd5ae14548cecab1
Pjotr Savitski <gnum@tlu.ee>
2016-04-15 12:52:20 +0300

    LL-1342 #resolve #close Fixed HTML positioning.
    Also made a small refactoring for the geenrated HTML, split a big
    repetitive portion into a standalone function.

--- help/js/app.js
+++ help/js/app.js
@@ -1,5 +1,23 @@
 define(['underscore', 'jquery', 'text!../data/help.json', 'text!../js/templates/navbar.tpl', 'text!../js/templates/overview_and_structure.tpl', 'colorbox', 'bootstrap'],
 function(_, $, help, NavbarTemplate, OverviewAndStructureTemplate) {
+    var generateVideoListItemHtml = function(video) {
+        var listClasses = ['list-group-item'],
+            html = '';
+
+        if ( video.url === 'NONE' ) {
+            listClasses.push('disabled');
+        }
+        html += '<li class="' + listClasses.join(' ') + '">';
+        html += '<span class="badge"><i class="fa fa-clock-o"></i> ' + video.duration + '</span>';
+        html += '<a href="' + video.url + '" class="youtube">';
+        html += '<i class="fa fa-youtube"></i>';
+        html += video.title;
+        html += '</a>';
+        html += '</li>';
+
+        return html;
+    };
+
     $(document).ready(function(){
         var helpData = JSON.parse(help),
             appContainerElement = $(document).find('#bnp-help-app'),
@@ -26,18 +44,7 @@ function(_, $, help, NavbarTemplate, OverviewAndStructureTemplate) {
                 if ( chapter.videos && _.isArray(chapter.videos) && chapter.videos.length > 0 ) {
                     helpText += '<ul class="list-group">';
                     _.each(chapter.videos, function(video) {
-                        var listClasses = ['list-group-item'];
-
-                        if ( video.url === 'NONE' ) {
-                            listClasses.push('disabled');
-                        }
-                        helpText += '<li class="' + listClasses.join(' ') + '">';
-                        helpText += '<a href="' + video.url + '" class="youtube">';
-                        helpText += '<i class="fa fa-youtube"></i>';
-                        helpText += video.title;
-                        helpText += '</a>';
-                        helpText += '<span class="badge"><i class="fa fa-clock-o"></i> ' + video.duration + '</span>';
-                        helpText += '</li>';
+                        helpText += generateVideoListItemHtml(video);
                     });
                     helpText += '</ul>';
                 }
